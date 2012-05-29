0552e508766bae53b0b47f08553320327e31583a
jlara91 <jlara91@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-05-29 16:18:06 +0000

    Clase Bombita

---
+++  tp-algoritmos-tres/TP2/src/Bombita.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP2.src
+{
+    class Bombita
+    {
+        private int cantidadDeBombas;
+        private int vida;
+        private int velocidad;
+
+        public Bombita(int cantidadDeBombas)
+        {
+            this.cantidadDeBombas = cantidadDeBombas;
+            this.vida = 1;
+            this.velocidad = 5;
+        }
+
+        public int CantidadDeBombas
+        {
+            get { return this.cantidadDeBombas;}
+        }
+
+        public void LanzarBomba()
+        {
+            this.cantidadDeBombas = this.cantidadDeBombas - 1;
+        }
+
+        public object Vida
+        {
+            get { return this.vida;}
+        }
+
+        public object Velocidad 
+        {
+            get { return this.velocidad ;} 
+        }
+
+
+    }
+}
