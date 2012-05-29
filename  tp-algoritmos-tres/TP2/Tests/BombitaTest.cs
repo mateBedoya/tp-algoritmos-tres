4bcc536b8c984afa9911cfcae533530ca8c037db
jlara91 <jlara91@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-05-29 16:18:41 +0000

    Clase de pruebas de Bombita

---
+++  tp-algoritmos-tres/TP2/Tests/BombitaTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TP2.src;
+
+namespace TP2.Tests
+{
+    [TestFixture]
+    class BombitaTest
+    {
+        [Test]
+        public void CuandoBombitaLanceUnaBombaSeLeResteUna()
+        {
+            Bombita bombita = new Bombita(5);
+
+            bombita.LanzarBomba();
+
+            Assert.AreEqual(bombita.CantidadDeBombas, 4);
+                    
+        }
+
+        [Test]
+        public void CuandoCreoABombitaQueTenga1Vida()
+        {
+            Bombita bombita = new Bombita(5);
+
+            Assert.AreEqual(bombita.Vida, 1);
+        }
+
+        [Test]
+        public void CuandoCreoABombitaQueTenga5DeVelocidad() //vemos despues que significa tener "5" de velocidad
+        {
+            Bombita bombita = new Bombita(0);
+
+            Assert.AreEqual(5, bombita.Velocidad);
+        }
+
+    }
+}
