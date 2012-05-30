9b7113f1c5b5dd16fcdb17da6987e6a84f7b1168
alan.rp26@gmail.com <alan.rp26@gmail.com@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-05-30 13:57:25 +0000

    Pruebas de la fisica de las entidades

---
+++ trunk/ServiciosTest/ServiciosTest.cs
@@ -0,0 +1,29 @@
+﻿using TP2.Elementales;
+using TP2.Fisica;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TP__Tests
+{
+    /// <summary>
+    ///Se trata de una clase de prueba para ServiciosTest y se pretende que
+    ///contenga todas las pruebas unitarias ServiciosTest.
+    ///</summary>
+   
+    [TestClass()]
+    public class ServiciosTest
+    {
+        private Servicios servicios = Servicios.GetInstancia();
+
+        // verifica la solicitud de servicios direccionales
+        [TestMethod()]
+        public void TestSolicitarDireccionesPosibles()
+        {
+            // asserts
+            Assert.AreEqual(servicios.DIRECCION_ESTE, 0.0);
+            Assert.AreEqual(servicios.DIRECCION_NORTE, 90.0);
+            Assert.AreEqual(servicios.DIRECCION_OESTE, 180.0);
+            Assert.AreEqual(servicios.DIRECCION_SUR, 270.0);
+        }
+    }
+}
