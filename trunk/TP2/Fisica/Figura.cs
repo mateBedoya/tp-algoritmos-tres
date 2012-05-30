aa6536ca07fde27160bda63faf9722011acce577
alan.rp26@gmail.com <alan.rp26@gmail.com@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-05-30 13:55:18 +0000

    Clases bases para la fisica y toda entidad

---
+++ trunk/TP2/Fisica/Figura.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP2.Fisica
+{
+    public abstract class Figura : Intersecable
+    {
+        protected Punto posicion;
+
+        // crea una figura posicionada en el origen del plano XY
+        public Figura()
+        {
+            this.posicion = new Punto();
+        }
+
+        // crea una figura asignandole una posicion dada en el plano XY
+        public Figura(Punto posicion)
+        {
+            this.posicion = posicion;
+        }
+
+        // posiciona a la figura 
+        public void Posicionar(Punto posicion)
+        {
+            this.posicion = posicion;
+        }
+
+        // retorna la posicion actual de la figura
+        public Punto GetPosicion()
+        {
+            return (this.posicion);
+        }
+
+        // se traslada por el plano XY
+        public void Trasladar(double deltaX, double deltaY)
+        {
+            this.posicion.Desplazar(deltaX, deltaY);
+        }
+
+        // cada figura en particular debe implementar si se encuentra 
+        // o no dentro de los limites pasados
+        public abstract bool EstaDentroDeLosLimites(Punto inferior, Punto superior);
+
+        ///
+        /// <Implementacion de la interfaz Intersecable>
+        /// 
+
+        // implementacion de la interfaz intersecable
+        public abstract bool SeIntersecaConElCirculo(Figura figura);
+
+        /// </Implementacion>
+
+    }
+}
---
+++ trunk/TP2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TP2
+{
+    static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+            List<int> enteros = new List<int>();
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(4);
+            enteros.Add(2);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            enteros.Add(0);
+            List<int> copiaDeEnteros = enteros; 
+
+            int indice = copiaDeEnteros.Count() - 1;
+            while (indice >= 0)
+            {
+                if (copiaDeEnteros[indice] == 0)
+                {
+                    enteros.RemoveAt(indice);
+                    enteros.Insert(indice, 8);
+                }
+                indice--;
+            }
+            foreach (int entero in enteros)
+            {
+                Console.WriteLine(entero);
+            }
+           
+           
+      
+            
+            
+           
+
+        }
+    }
+}
