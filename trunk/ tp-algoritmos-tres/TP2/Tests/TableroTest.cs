81e429216ba1ebb4d9220d7850b883c8bdc89ccc
jlara91 <jlara91@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-06-01 03:14:59 +0000

    Agregue la prueba PruebaQueDevuelvaLosAdyacentesDeUnCasillero

--- trunk/ tp-algoritmos-tres/TP2/Tests/TableroTest.cs
+++ trunk/ tp-algoritmos-tres/TP2/Tests/TableroTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using System.Collections;
 
 namespace TP2.Tests
 {
@@ -14,7 +15,7 @@ namespace TP2.Tests
         {
             Tablero tablero = new Tablero(1, 1);
 
-            Assert.AreEqual(tablero.Tamanio, 1);
+            Assert.AreEqual(1, tablero.Tamanio);
         }
 
         [Test]
@@ -24,9 +25,36 @@ namespace TP2.Tests
 
             Casillero casillero = tablero.ObtenerCasillero(1, 2);
 
-            Assert.AreEqual(casillero.Columna,2);
-            Assert.AreEqual(casillero.Fila, 1);
-            
+            Assert.AreEqual(2,casillero.Columna);
+            Assert.AreEqual(1,casillero.Fila);
+        }
+
+        [Test]
+        public void PruebaQueDevuelvaLosAdyacentesDeUnCasillero()
+        {
+            Tablero tablero = new Tablero(4, 4);
+
+            ArrayList listaDeAdyacentes = tablero.ObtenerAdyacentes(2, 2); // izq,arriba,der,abajo
+
+            Casillero izquierda = (Casillero)listaDeAdyacentes[0];
+            Casillero arriba = (Casillero)listaDeAdyacentes[1];
+            Casillero derecha = (Casillero)listaDeAdyacentes[2];
+            Casillero abajo = (Casillero)listaDeAdyacentes[3];
+
+            Assert.AreEqual(1, izquierda.Columna);
+            Assert.AreEqual(2, izquierda.Fila);
+            Assert.AreEqual(2, arriba.Columna);
+            Assert.AreEqual(1, arriba.Fila);
+            Assert.AreEqual(3, derecha.Columna);
+            Assert.AreEqual(2, derecha.Fila);
+            Assert.AreEqual(2, abajo.Columna);
+            Assert.AreEqual(3, abajo.Fila);
         }
     }
 }
+
+//  0 1 2 3
+//0 1 1 1 1
+//1 1 1 1 1
+//2 1 1 1 1
+//3 1 1 1 1
