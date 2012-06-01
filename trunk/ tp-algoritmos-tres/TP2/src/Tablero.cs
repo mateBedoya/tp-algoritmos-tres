615b60369a35abbffd0415de0c451afe8873011c
jlara91 <jlara91@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-06-01 03:14:27 +0000

    Agregue el metodo ObtenerAdyacentes

--- trunk/ tp-algoritmos-tres/TP2/src/Tablero.cs
+++ trunk/ tp-algoritmos-tres/TP2/src/Tablero.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace TP2
 {
@@ -32,5 +33,22 @@ namespace TP2
         {
             return tablero[x, y];
         }
+
+        public ArrayList ObtenerAdyacentes(int fila, int columna) //Devuelve el izquierdo, despues el de arriba, despues el derecho, y despues el de abajo
+        {
+            ArrayList listaDeAdyacentes = new ArrayList();
+            listaDeAdyacentes.Add(tablero[fila,columna-1]);//izq
+            listaDeAdyacentes.Add(tablero[fila-1,columna]);//arriba
+            listaDeAdyacentes.Add(tablero[fila,columna+1]);//der
+            listaDeAdyacentes.Add(tablero[fila+1,columna]);//abajo
+
+            return listaDeAdyacentes;
+        }
     }
 }
+
+//  0 1 2 3
+//0 1 1 1 1
+//1 1 1 1 1
+//2 1 1 1 1
+//3 1 1 1 1
