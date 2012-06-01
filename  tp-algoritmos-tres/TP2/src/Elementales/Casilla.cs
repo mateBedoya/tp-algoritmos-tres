5bacbeae3beecd7cfad4d024f048b0c2ec4f3ec1
alan.rp26 <alan.rp26@gmail.com>
2012-06-01 17:28:40 +0000

    Creo una estructura mas "feliz"

---
+++  tp-algoritmos-tres/TP2/src/Elementales/Casilla.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TP2.Juego.personajes;
+using TP2.Juego.obstaculos;
+using TP2.Juego.bombas;
+using TP2.Juego.articulos;
+using TP2.Excepciones;
+
+namespace TP2.Elementales
+{
+    public class Casilla
+    {
+        private int fila;
+        private int columna;
+        private Personaje personaje;
+        private Obstaculo obstaculo;
+        private Bomba bomba;
+        private Articulo articulo;
+
+        // crea una casilla vacia con una posicion en el tablero
+        public Casilla(int fila, int columna)
+        {
+            this.fila = fila;
+            this.columna = columna;
+            this.personaje = PersonajeNull.GetInstancia();
+            this.obstaculo = ObstaculoNull.GetInstancia();
+            this.bomba = BombaNull.GetInstancia();
+            this.articulo = ArticuloNull.GetInstancia();
+        }
+
+        // retorna la fila que ocupa en el tablero
+        public int GetFila()
+        {
+            return (this.fila);
+        }
+
+        // retorna la columna que ocupa en el tablero
+        public int GetColumna()
+        {
+            return (this.columna);
+        }
+
+        // retorna la casilla adyacente- superior 
+        public Casilla GetCasillaSuperior()
+        { 
+            return (Tablero.GetInstancia().GetCasilla(this.fila + 1, this.columna));
+        }
+
+        // retorna la casilla adyacente- inferior
+        public Casilla GetCasillaInferior()
+        {
+            return (Tablero.GetInstancia().GetCasilla(this.fila - 1, this.columna));
+        }
+
+        // retorna la casilla adyacente- derecha
+        public Casilla GetCasillaDerecha()
+        {
+            return (Tablero.GetInstancia().GetCasilla(this.fila, this.columna + 1));
+        }
+
+        // retorna la casilla adyacente- izquierda
+        public Casilla GetCasillaIzquierda()
+        {
+           return (Tablero.GetInstancia().GetCasilla(this.fila, this.columna - 1));  
+        }
+
+        // retorna si la casilla esta vacia
+        public bool EstaVacia()
+        {
+            return (this.personaje == PersonajeNull.GetInstancia() & 
+                    this.obstaculo == ObstaculoNull.GetInstancia() &
+                    this.bomba == BombaNull.GetInstancia() & 
+                    this.articulo == ArticuloNull.GetInstancia());
+        }
+    }
+}
---
+++  tp-algoritmos-tres/TP2/src/Juego.bombas/BombaNull.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP2.Juego.bombas
+{
+    /// <summary>
+    /// Esta clase solo es utilizada para inicializar un objeto
+    /// de tipo bomba en nulo (patron null object)
+    /// </summary>
+    public class BombaNull : Bomba
+    {
+        private static BombaNull INSTANCIA = null;
+
+        // solo puede crearse una sola instancia de esta clase
+        private BombaNull()
+            : base() { }
+
+        // retorna la instancia
+        public static BombaNull GetInstancia()
+        {
+            if (INSTANCIA == null)
+                INSTANCIA = new BombaNull();
+            return (INSTANCIA);
+        }
+    }
+}
