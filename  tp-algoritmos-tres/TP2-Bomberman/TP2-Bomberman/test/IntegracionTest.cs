d21c36f84cdeb33a381022ab7d7977170d634013
jlara91 <jlara91@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-06-05 05:15:17 +0000

    Agrego la clase de prueba IntegracionTest.cs y dejo sin funcionar una prueba para el choque entre bombita y un enemigo para seguir trabajando mañana

---
+++  tp-algoritmos-tres/TP2-Bomberman/TP2-Bomberman/test/IntegracionTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TP2_Bomberman.src;
+
+namespace TP2_Bomberman.test
+{
+    [TestFixture]
+    class IntegracionTest
+    {
+        [Test]
+        public void CreaUnTableroConUnBombitaYUnCecilioYLosMueve()
+        {
+            Tablero tablero = new Tablero();
+            Bombita bombita = new Bombita(); // Arranca en (0,0)
+            Cecilio cecilio = new Cecilio(tablero.ObtenerCasillero(5,0));
+
+            bombita.MoverAbajo();
+            cecilio.MoverArriba();
+            cecilio.MoverArriba();
+            cecilio.MoverArriba();
+
+            Assert.AreEqual(1, bombita.Posicion.Fila);
+            Assert.AreEqual(2, cecilio.Posicion.Fila);
+        }
+
+        [Test]
+        public void CreaUnTableroConUnBombitaYUnCecilioYLosMueveHastaChocarse()
+        {
+            Tablero tablero = new Tablero();
+            Bombita bombita = new Bombita(); // Arranca en (0,0)
+            Cecilio cecilio = new Cecilio(tablero.ObtenerCasillero(5, 0));
+
+            bombita.MoverAbajo();
+            cecilio.MoverArriba();
+            cecilio.MoverArriba();
+            cecilio.MoverArriba();
+            cecilio.MoverArriba();
+
+            Assert.IsTrue(bombita.FueDestruido());
+            Assert.IsFalse(cecilio.FueDestruido());
+            Assert.AreEqual(1, cecilio.Posicion.Fila);
+            Assert.AreEqual(0, cecilio.Posicion.Columna);
+            
+        }
+
+    }
+}
