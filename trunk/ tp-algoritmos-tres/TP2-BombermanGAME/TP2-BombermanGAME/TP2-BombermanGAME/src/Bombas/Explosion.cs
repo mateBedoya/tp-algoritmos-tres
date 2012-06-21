a8767c8aa51c3be514ddef920e093e863c491892
jlara91 <jlara91@ef8e4f00-7889-5a3b-f325-562ed602a130>
2012-06-21 01:47:53 +0000

    Agregue el Explosion.cs

---
+++ trunk/ tp-algoritmos-tres/TP2-BombermanGAME/TP2-BombermanGAME/TP2-BombermanGAME/src/Bombas/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TP2_Bomberman.src;
+using TP2_Bomberman.src.Elementales;
+using TP2_Bomberman.src.Interfaces;
+
+namespace TP2_BombermanGAME.src.Bombas
+{
+    public class Explosion: Entidad, IDependienteDelTiempo
+    {
+        public Bomba bomba;
+        private double tiempoTranscurrido = 0;
+
+        public Explosion(Bomba bomba)
+        {
+            this.bomba = bomba;
+        }
+        
+        public void CuandoPaseElTiempo(double tiempo)
+        {
+            tiempoTranscurrido = tiempoTranscurrido + tiempo;
+            if (tiempoTranscurrido >= 1)
+            {
+                this.posicion = null;
+            }
+        }
+    }
+}
